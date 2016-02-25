7c4d0d838d479252daf9ace18edafdfdddfc09b8
SeanMcGrath <srmcgrat@umass.edu>
2016-02-25 17:04:05 -0500

    added make build system

--- macros/integrateMacro.c
+++
@@ -1,8 +0,0 @@
-void integrateMacro(char * fileName){
-	gROOT->ProcessLine(".L integrate_peaks.c");
-	gROOT->ProcessLine("integrate_peaks scan;");
-	TFile *f = new TFile(fileName);
-	TTree *t = (TTree*)f; 
-	gROOT->ProcessLine("Df125WindowRawData->Process(&scan)");
-	f->Close();
-}
